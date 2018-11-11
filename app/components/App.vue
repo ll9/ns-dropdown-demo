071d9e4127d7c0a798640a1a3cae302810999482
ll9 <llwierer@web.de>
2018-11-11 16:20:46 +0100

    init

---
+++ app/components/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <Page>
+<StackLayout>
+    <GridLayout rows="auto, auto, *"
+                columns="auto, *">
+        <DropDown ref="dd"
+                  backroundColor="red"
+                  :items="items"
+                  :selectedIndex="selectedIndex"
+                  @selectedIndexChanged="onchange"
+                  @opened="onopen"
+                  @closed="onclose"
+                  row="0"
+                  colSpan="2"></DropDown>
+        <Label text="Selected Index:"
+               row="1"
+               col="0"
+               fontSize="18"
+               verticalAlignment="bottom"></Label>
+        <Label :text="selectedIndex"
+                   row="1"
+                   col="1"></Label>
+    </GridLayout>
+</StackLayout>
+    </Page>
+</template>
+
+<script>
+import { SelectedIndexChangedEventData } from "nativescript-drop-down";
+
+export default {
+  data() {
+    return {
+      selectedIndex: 1,
+      items: []
+    };
+  },
+  mounted() {
+    this.items = [];
+    for (var i = 0; i < 5; i++) {
+      this.items.push("data item " + i);
+    }
+  },
+  methods: {
+    onchange(args) {
+      this.selectedIndex = args.newIndex;
+      console.log(
+        `Drop Down selected index changed from ${args.oldIndex} to ${
+          args.newIndex
+        }`
+      );
+    },
+
+    onopen() {
+      console.log("Drop Down opened.");
+    },
+
+    onclose() {
+      console.log("Drop Down closed.");
+    }
+  }
+};
+</script>
+
+<style scoped>
+Dropdown {
+  padding: 20;
+}
+</style>
---
+++ app/main.js
@@ -0,0 +1,17 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+import VueDevtools from 'nativescript-vue-devtools'
+
+Vue.registerElement("DropDown", () => require("nativescript-drop-down/drop-down").DropDown);
+
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools)
+}
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+
+new Vue({
+  render: h => h('frame', [h(App)])
+}).$start()
